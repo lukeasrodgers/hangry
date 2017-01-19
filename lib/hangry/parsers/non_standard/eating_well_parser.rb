3320e8351d6703bdb1e9ab04a3ed0059385e3bbe
Luke Rodgers <lukeasrodgers@gmail.com>
2017-01-19 13:29:39 -0500

    fix eating_well_parser

--- lib/hangry/parsers/non_standard/eating_well_parser.rb
+++ lib/hangry/parsers/non_standard/eating_well_parser.rb
@@ -4,7 +4,7 @@ module Hangry
       class EatingWellParser < SchemaOrgRecipeParser
 
         def self.root_selector
-          '[itemtype="http://schema.org/recipe"]'
+          '[itemtype="http://schema.org/Recipe"]'
         end
 
         def self.can_parse?(html)
@@ -12,27 +12,31 @@ module Hangry
         end
 
         def parse_instructions
-          nodes_with_itemprop(:recipeinstructions).map(&:content).join("\n")
+          nodes_with_itemprop(:recipeInstructions).map(&:content).join("\n")
         end
 
         def parse_nutrition
           recipe.nutrition.tap do |nutrition|
             nutrition[:calories] = nutrition_property_value(:calories)
-            nutrition[:cholesterol] = nutrition_property_value(:cholesterolcontent)
-            nutrition[:fiber] = nutrition_property_value(:fibercontent)
-            nutrition[:protein] = nutrition_property_value(:proteincontent)
-            nutrition[:saturated_fat] = nutrition_property_value(:saturatedfatcontent)
-            nutrition[:sodium] = nutrition_property_value(:sodiumcontent)
-            nutrition[:sugar] = nutrition_property_value(:sugarcontent)
-            nutrition[:total_carbohydrates] = nutrition_property_value(:carbohydratecontent)
-            nutrition[:total_fat] = nutrition_property_value(:fatcontent)
-            nutrition[:trans_fat] = nutrition_property_value(:transfatcontent)
-            nutrition[:unsaturated_fat] = nutrition_property_value(:unsaturatedfatcontent)
+            nutrition[:cholesterol] = nutrition_property_value(:cholesterolContent)
+            nutrition[:fiber] = nutrition_property_value(:fiberContent)
+            nutrition[:protein] = nutrition_property_value(:proteinContent)
+            nutrition[:saturated_fat] = nutrition_property_value(:saturatedFatContent)
+            nutrition[:sodium] = nutrition_property_value(:sodiumContent)
+            nutrition[:sugar] = nutrition_property_value(:sugarContent)
+            nutrition[:total_carbohydrates] = nutrition_property_value(:carbohydrateContent)
+            nutrition[:total_fat] = nutrition_property_value(:fatContent)
+            nutrition[:trans_fat] = nutrition_property_value(:transfatContent)
+            nutrition[:unsaturated_fat] = nutrition_property_value(:unsaturatedFatContent)
           end
         end
 
         def parse_yield
-          value(node_with_itemprop(:recipeyield).content) || NullObject.new
+          value(node_with_itemprop(:recipeYield)['content']) || NullObject.new
+        end
+
+        def parse_description
+          nokogiri_doc.css('meta[itemprop="description"]').first['content']
         end
 
       end
--- spec/real_examples/schema_org/eating_well_spec.rb
+++ spec/real_examples/schema_org/eating_well_spec.rb
@@ -11,37 +11,31 @@ describe Hangry do
       expect(Hangry::ParserClassSelecter.new(html).parser_class).to eq(Hangry::Parsers::NonStandard::EatingWellParser)
     end
 
-    its(:author) { should == "From EatingWell" }
-    its(:canonical_url) { should == "http://www.eatingwell.com/recipes/sauteed_chicken_breasts_with_creamy_chive_sauce.html" }
-    its(:cook_time) { should == nil }
+    its(:author) { should == "EatingWell Test Kitchen" }
+    its(:canonical_url) { should == "http://www.eatingwell.com/recipe/251798/sauteed-chicken-breasts-with-creamy-chive-sauce/" }
+    its(:cook_time) { should == 35 }
     its(:description) { should == "Here's a sauce so delicious, it's missing only one thing: a little crunchy bread to dip in it. Make It a Meal: Serve with steamed asparagus or cauliflower, mashed potatoes or orzo pasta, and a glass of Vinho Verde." }
-    its(:image_url) { should == "http://bed56888308e93972c04-0dfc23b7b97881dee012a129d9518bae.r34.cf1.rackcdn.com/sites/default/files/imagecache/standard/recipes/MP4834.JPG" }
+    its(:image_url) { should == "http://images.media-allrecipes.com/userphotos/960x960/3755910.jpg" }
     its(:ingredients) {
       should == [
         "4 boneless, skinless chicken breasts, (about 1 pound), trimmed of fat",
-        "1 teaspoon kosher salt, divided",
-        "1/4 cup plus 1 tablespoon all-purpose flour, divided",
-        "3 teaspoons extra-virgin olive oil, divided",
-        "2 large shallots, finely chopped",
-        "1/2 cup dry white wine",
-        "1 14-ounce can reduced-sodium chicken broth",
-        "1/3 cup reduced-fat sour cream",
-        "1 tablespoon Dijon mustard",
-        "1/2 cup chopped chives, (about 1 bunch)"
-      ]
+        "1 teaspoon kosher salt, divided", "¼ cup plus 1 tablespoon all-purpose flour, divided",
+        "3 teaspoons extra-virgin olive oil, divided", "2 large shallots, finely chopped",
+        "½ cup dry white wine", "1 14-ounce can reduced-sodium chicken broth", "⅓ cup reduced-fat sour cream",
+        "1 tablespoon Dijon mustard", "½ cup chopped chives, (about 1 bunch)"]
     }
     its(:name) { should == "Sauteed Chicken Breasts with Creamy Chive Sauce" }
     its(:nutrition) do
       should == {
-        calories: '244',
-        cholesterol: '72 mg',
-        fiber: '0 g',
-        protein: '26 g',
-        saturated_fat: '3 g',
-        sodium: '679 mg',
-        sugar: nil,
-        total_carbohydrates: '1 g',
-        total_fat: '9 g',
+        calories: '257 calories;',
+        cholesterol: '70',
+        fiber: '0.0',
+        protein: '26.0',
+        saturated_fat: '3.0',
+        sodium: '634',
+        sugar: '1.0',
+        total_carbohydrates: '12.0',
+        total_fat: '9.0',
         trans_fat: nil,
         unsaturated_fat: nil
       }
@@ -49,18 +43,18 @@ describe Hangry do
 
     its(:instructions) {
       instructions = <<-EOS
-Place chicken between sheets of plastic wrap and pound with a meat mallet or heavy skillet until flattened to an even thickness, about 1/2 inch. Season both sides of the chicken with 1/2 teaspoon salt. Place 1/4 cup flour in a shallow glass baking dish and dredge the chicken in it. Discard the excess flour.
+Place chicken between sheets of plastic wrap and pound with a meat mallet or heavy skillet until flattened to an even thickness, about ½ inch. Season both sides of the chicken with ½ teaspoon salt. Place ¼ cup flour in a shallow glass baking dish and dredge the chicken in it. Discard the excess flour.
 Heat 2 teaspoons oil in a large nonstick skillet over medium-high heat. Add the chicken and cook until golden brown, 1 to 2 minutes per side. Transfer to a plate, cover and keep warm.
-Heat the remaining 1 teaspoon oil in the pan over medium-high heat. Add shallots and cook, stirring constantly and scraping up any browned bits, until golden brown, 1 to 2 minutes. Sprinkle with the remaining 1 tablespoon flour; stir to coat. Add wine, broth and the remaining 1/2 teaspoon salt; bring to a boil, stirring often.
+Heat the remaining 1 teaspoon oil in the pan over medium-high heat. Add shallots and cook, stirring constantly and scraping up any browned bits, until golden brown, 1 to 2 minutes. Sprinkle with the remaining 1 tablespoon flour; stir to coat. Add wine, broth and the remaining ½ teaspoon salt; bring to a boil, stirring often.
 Return the chicken and any accumulated juices to the pan, reduce heat to a simmer, and cook until heated through and no longer pink in the center, about 6 minutes. Stir in sour cream and mustard until smooth; turn the chicken to coat with the sauce. Stir in chives and serve immediately.
       EOS
       should == instructions.strip
     }
 
-    its(:prep_time) { should == 35 }
+    its(:prep_time) { should == nil }
     its(:published_date) { should == nil }
     its(:total_time) { should == 35 }
-    its(:yield) { should == "4 servings" }
+    its(:yield) { should == "4" }
 
   end
 
