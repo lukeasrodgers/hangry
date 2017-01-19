0f03569c43b01adbb450d813c5be2eb9ac716be9
Luke Rodgers <lukeasrodgers@gmail.com>
2017-01-19 13:17:28 -0500

    fix betty_crocker_spec

--- spec/real_examples/schema_org/betty_crocker_spec.rb
+++ spec/real_examples/schema_org/betty_crocker_spec.rb
@@ -4,26 +4,23 @@ require 'hangry'
 describe Hangry do
 
   context "bettycrocker.com recipe" do
-    let(:html) { File.read("spec/fixtures/schema_org/betty_crocker.html") }
-    subject { Hangry.parse(html) }
+    before(:all) do
+      @html = File.read("spec/fixtures/schema_org/betty_crocker.html")
+      @parsed = Hangry.parse(@html)
+    end
+    subject { @parsed }
 
     its(:author) { should == nil }
     its(:canonical_url) { should == "http://www.bettycrocker.com/recipes/skillet-chicken-nachos/9bf0c3be-09dd-4b1b-8cf4-a9cfa979b232" }
     its(:cook_time) { should == nil }
     its(:description) { should == "Dinner ready in 20 minutes! Enjoy these delicious nachos spooned with chicken mixture sprinkled with cheese and cilantro - perfect for Mexican cuisine." }
-    its(:image_url) { should == 'http://s3.amazonaws.com/gmi-digital-library/ea8c5a0a-da18-45cc-a1f2-c112643dfd56.jpg' }
+    its(:image_url) { should == 'http://images-gmi-pmc.edge-generalmills.com/5831d445-1435-4c97-b494-0b9c6d0e859b.jpg' }
     its(:ingredients) {
       should == [
-        "1 tablespoon olive or vegetable oil",
-        "1 1/4 lb boneless skinless chicken breasts, cut into 1/4-inch pieces",
-        "1 package (1 oz) Old El Paso™ taco seasoning mix",
-        "1 can (8 oz) tomato sauce",
-        "1 medium red bell pepper, chopped (1 cup)",
-        "1 can (15 oz) Progresso™ black beans, drained, rinsed",
-        "1 can (7 oz) Green Giant™ Niblets® whole kernel sweet corn, drained",
-        "2 cups shredded Mexican cheese blend (8 oz)",
-        "6 oz tortilla chips (about 42 chips)",
-        "1/4 cup chopped fresh cilantro"
+        "1 tablespoon olive or vegetable oil", "1 1/4 lb boneless skinless chicken breasts, cut into 1/4-inch pieces",
+        "1 package (1 oz) Old El Paso™ taco seasoning mix", "1 can (8 oz) tomato sauce", "1 medium red bell pepper, chopped (1 cup)",
+        "1 can (15 oz) Progresso™ black beans, drained, rinsed", "1 can (7 oz) whole kernel sweet corn, drained",
+        "2 cups shredded Mexican cheese blend (8 oz)", "6 oz tortilla chips (about 42 chips)", "1/4 cup chopped fresh cilantro"
       ]
     }
     its(:name) { should == "Skillet Chicken Nachos" }
@@ -54,7 +51,7 @@ Divide tortilla chips between 6 plates. Spoon chicken mixture evenly over chips.
       EOS
       should == instructions.strip
     }
-    its(:prep_time) { should == nil }
+    its(:prep_time) { should == 20 }
     its(:published_date) { should == nil }
     its(:total_time) { should == 20 }
     its(:yield) { should == "6" }
