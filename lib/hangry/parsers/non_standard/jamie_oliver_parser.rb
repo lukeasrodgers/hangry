f893723a1928955e1a3bbdf5b84560137d23e0eb
Luke Rodgers <lukeasrodgers@gmail.com>
2017-01-18 23:20:03 -0500

    Use JsonLD parser for Jamie Oliver.

--- lib/hangry/parsers/non_standard/jamie_oliver_parser.rb
+++ lib/hangry/parsers/non_standard/jamie_oliver_parser.rb
@@ -1,18 +1,27 @@
 module Hangry
   module Parsers
     module NonStandard
-      class JamieOliverParser < HRecipeParser
+      class JamieOliverParser < JsonLDParser
 
         def self.can_parse?(html)
-          html.include?('jamieoliver.com')
+          canonical_url_matches_domain?(html, 'jamieoliver.com')
         end
 
-        def parse_description
-          recipe_ast.css(".instructions").css("i").first.content
+        private
+
+        def parse_author
+          "Jamie Oliver"
         end
 
         def parse_instructions
-          recipe_ast.css(".content").css(".instructions").map(&:content).join("\n") 
+          # Some sites like may have their recipe instructions doubled if they
+          # support different ways of presentation.
+          # E.g. http://www.pillsbury.com/recipes/big-cheesy-pepperoni-pockets/a17766e6-30ce-4a0c-af08-72533bb9b449
+          # has its steps doubled ("step by step" and "list" modes).
+          nodes = nodes_with_itemprop(:recipeInstructions)
+          nodes = [nodes].flatten
+          html = nodes.map(&:strip).uniq.join("\n")
+          Nokogiri::HTML(html.gsub(/<\/li><li>/, "\n")).content
         end
       end
     end
