104dfe018245c40fa6ee1216d9c9155e214b0aa0
Luke Rodgers <lukeasrodgers@gmail.com>
2017-01-19 19:17:37 -0500

    Fix food_and_wine_spec

--- spec/real_examples/schema_org/food_and_wine_spec.rb
+++ spec/real_examples/schema_org/food_and_wine_spec.rb
@@ -4,14 +4,17 @@ require 'hangry'
 describe Hangry do
 
   context "foodandwine.com recipe" do
-    let(:html) { File.read("spec/fixtures/schema_org/foodandwine.com.html") }
-    subject { Hangry.parse(html) }
+    before(:all) do
+      @html = File.read("spec/fixtures/schema_org/foodandwine.com.html")
+      @parsed = Hangry.parse(@html)
+    end
+    subject { @parsed }
 
     its(:author) { should == "Grace Parisi" }
     its(:canonical_url) { should == "http://www.foodandwine.com/recipes/honey-glazed-roasted-root-vegetables" }
-    its(:cook_time) { should == 25 }
-    its(:description) { should == "The secret to this sweet, slightly tangy dish: the touch of sherry vinegar in the glaze." }
-    its(:image_url) { should == "http://www.foodandwine.com/images/sys/fw200711_r_gpbrocrootveg.jpg" }
+    its(:cook_time) { should == nil }
+    its(:description) { should == "The secret to this sweet, slightly tangy dish: the touch of sherry vinegar in the glaze. Slideshow: More Thanksgiving Side Dishes" }
+    its(:image_url) { should == "http://cdn-image.foodandwine.com/sites/default/files/styles/551x551/public/200711-xl-honey-glazed-roasted-root-vegetables.jpg?itok=VlSDyVrG" }
     its(:ingredients) {
       should == [
         "1 1/4 pounds parsnips, peeled and sliced 1/2 inch thick", "1 1/4 pounds carrots, peeled and sliced 1/2 inch thick", "One 1 1/4 pound celery root—peeled, quartered and sliced 1/2 inch thick", "1 1/4 pounds golden beets, peeled and sliced 1/2 inch thick", "1/2 cup extra-virgin olive oil", "1/2 cup honey", "6 thyme sprigs", "Salt and freshly ground pepper", "2 tablespoons sherry vinegar"
@@ -36,11 +39,11 @@ describe Hangry do
 
     its(:instructions) {
       instructions = <<-EOS
-Preheat the oven to 425°. In a large bowl, toss the root vegetables with the oil, honey and thyme and season with salt and pepper. Divide between 2 large, sturdy rimmed baking sheets. Cover with foil and roast for 40 minutes, shifting the pans once, until the vegetables are tender. Remove the foil and roast for 10 minutes longer, until glazed. Return them to the bowl and stir in the vinegar then season with salt and pepper. Serve right away.
+Preheat the oven to 425&#176;. In a large bowl, toss the root vegetables with the oil, honey and thyme and season with salt and pepper. Divide between 2 large, sturdy rimmed baking sheets. Cover with foil and roast for 40 minutes, shifting the pans once, until the vegetables are tender. Remove the foil and roast for 10 minutes longer, until glazed. Return them to the bowl and stir in the vinegar then season with salt and pepper. Serve right away.
       EOS
       should == instructions.strip
     }
-    its(:prep_time) { should == nil }
+    its(:prep_time) { should == 25 }
     its(:published_date) { should == nil }
     its(:total_time) { should == 90 }
     its(:yield) { should == "12" }
