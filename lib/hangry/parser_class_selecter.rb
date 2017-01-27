d1e96ab8fc968bb6c35092c8b67b4ccbe5c6735b
Luke Rodgers <lukeasrodgers@gmail.com>
2017-01-26 19:23:07 -0500

    Add rachael ray parser. Not yet tested extensively. Unfortunately, this site doesn't use schema.org or any semantic-web markup.

--- lib/hangry/parser_class_selecter.rb
+++ lib/hangry/parser_class_selecter.rb
@@ -14,6 +14,7 @@ require 'hangry/parsers/non_standard/jamie_oliver_parser'
 require 'hangry/parsers/non_standard/southern_food_parser'
 require 'hangry/parsers/non_standard/taste_of_home_parser'
 require 'hangry/parsers/non_standard/mr_food_com_parser'
+require 'hangry/parsers/non_standard/rachaelray_parser'
 
 module Hangry
   class ParserClassSelecter
@@ -32,7 +33,8 @@ module Hangry
         Parsers::NonStandard::JamieOliverParser,
         Parsers::NonStandard::SouthernFoodParser,
         Parsers::NonStandard::TasteOfHomeParser,
-        Parsers::NonStandard::MrFoodComParser
+        Parsers::NonStandard::MrFoodComParser,
+        Parsers::NonStandard::RachaelRayParser
       ]
       parser_classes += [SchemaOrgRecipeParser, HRecipeParser, DataVocabularyRecipeParser, JsonLDParser]
       parser_classes << DefaultRecipeParser
