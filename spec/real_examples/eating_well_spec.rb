a9e93bc4282c8075a0a4d693493dda15fdb843ab
Bob Lail <bob.lailfamily@gmail.com>
2017-07-29 09:08:15 -0500

    Destructured real examples
    I did this for four reasons:
    
    1. A number of examples were miscategorized which is like when a code comment is disagreeing with the the adjacent code.
    
    2. Recipe sites will change (and have) their use of microformats over time. Organizing the examples by microformat would require a step after running `rake update_fixtures` where the developer checks to see if the self-selected parser has changed and update the Rakefile, fixtures, and spec folders accordingly.
    
    3. Sites often use more than one microformat; and if we chose to reprioritize the list of standard parsers in Hangry::ParserSelector, fixtures and tests might be required to move.
    
    4. Since sites use more than one microformat, a potential improvement to Hangry might be to parse every site with all available microformats and then stitch together into one Recipe what can be gleaned from all of them.

---
+++ spec/real_examples/eating_well_spec.rb
@@ -0,0 +1,64 @@
+# encoding: UTF-8
+
+describe Hangry do
+
+  context "eatingwell.com recipe" do
+    before(:all) do
+      @html = File.read("spec/fixtures/eatingwell.com.html")
+      @parsed = Hangry.parse(@html)
+    end
+    subject { @parsed }
+
+    it "should use a non-standard parser" do
+      expect(Hangry::ParserSelector.new(@html).parser).to be_an_instance_of(Hangry::Parsers::NonStandard::EatingWellParser)
+    end
+
+    its(:author) { should == "EatingWell Test Kitchen" }
+    its(:canonical_url) { should == "http://www.eatingwell.com/recipe/251798/sauteed-chicken-breasts-with-creamy-chive-sauce/" }
+    its(:cook_time) { should == 35 }
+    its(:description) { should == "Here's a sauce so delicious, it's missing only one thing: a little crunchy bread to dip in it. Make It a Meal: Serve with steamed asparagus or cauliflower, mashed potatoes or orzo pasta, and a glass of Vinho Verde." }
+    its(:image_url) { should == "http://images.media-allrecipes.com/userphotos/960x960/3755910.jpg" }
+    its(:ingredients) {
+      should == [
+        "4 boneless, skinless chicken breasts, (about 1 pound), trimmed of fat",
+        "1 teaspoon kosher salt, divided", "¼ cup plus 1 tablespoon all-purpose flour, divided",
+        "3 teaspoons extra-virgin olive oil, divided", "2 large shallots, finely chopped",
+        "½ cup dry white wine", "1 14-ounce can reduced-sodium chicken broth", "⅓ cup reduced-fat sour cream",
+        "1 tablespoon Dijon mustard", "½ cup chopped chives, (about 1 bunch)"]
+    }
+    its(:name) { should == "Sauteed Chicken Breasts with Creamy Chive Sauce" }
+    its(:nutrition) do
+      should == {
+        calories: '257 calories;',
+        cholesterol: '70',
+        fiber: '0.0',
+        protein: '26.0',
+        saturated_fat: '3.0',
+        sodium: '634',
+        sugar: '1.0',
+        total_carbohydrates: '12.0',
+        total_fat: '9.0',
+        trans_fat: nil,
+        unsaturated_fat: nil
+      }
+    end
+
+    its(:instructions) {
+      instructions = <<-EOS
+Place chicken between sheets of plastic wrap and pound with a meat mallet or heavy skillet until flattened to an even thickness, about ½ inch. Season both sides of the chicken with ½ teaspoon salt. Place ¼ cup flour in a shallow glass baking dish and dredge the chicken in it. Discard the excess flour.
+Heat 2 teaspoons oil in a large nonstick skillet over medium-high heat. Add the chicken and cook until golden brown, 1 to 2 minutes per side. Transfer to a plate, cover and keep warm.
+Heat the remaining 1 teaspoon oil in the pan over medium-high heat. Add shallots and cook, stirring constantly and scraping up any browned bits, until golden brown, 1 to 2 minutes. Sprinkle with the remaining 1 tablespoon flour; stir to coat. Add wine, broth and the remaining ½ teaspoon salt; bring to a boil, stirring often.
+Return the chicken and any accumulated juices to the pan, reduce heat to a simmer, and cook until heated through and no longer pink in the center, about 6 minutes. Stir in sour cream and mustard until smooth; turn the chicken to coat with the sauce. Stir in chives and serve immediately.
+      EOS
+      should == instructions.strip
+    }
+
+    its(:prep_time) { should == nil }
+    its(:published_date) { should == nil }
+    its(:total_time) { should == 35 }
+    its(:yield) { should == "4" }
+
+  end
+
+end
+
---
+++ spec/real_examples/taste_com_au_spec.rb
@@ -0,0 +1,47 @@
+# encoding: UTF-8
+
+describe Hangry do
+
+  context "taste.com.au recipe" do
+    before(:all) do
+      @html =File.read("spec/fixtures/taste.com.au.html")
+      @parsed = Hangry.parse(@html)
+    end
+    subject { @parsed }
+
+    it "should use the data-vocabulary parser" do
+      expect(Hangry::ParserSelector.new(@html).parser).to be_an_instance_of(Hangry::DataVocabularyRecipeParser)
+    end
+
+    its(:author) { should == "Sophia Young" }
+    its(:canonical_url) { should == "/recipes/24586/lemon+melting+moments" }
+    its(:cook_time) { should == 15 }
+    its(:description) { should == "Take a trip down memory lane with these buttery biccies. They're great for teatime or with coffee for your next dinner party." }
+    its(:image_url) { should == "http://cdn.taste.com.au/images/recipes/mc/2010/07/24586_l.jpg" }
+    its(:ingredients) { should == [] }
+    its(:name) { should == "Lemon melting moments" }
+    its(:nutrition) do
+      should == {
+        calories: nil,
+        cholesterol: nil,
+        fiber: nil,
+        protein: nil,
+        saturated_fat: nil,
+        sodium: nil,
+        sugar: nil,
+        total_carbohydrates: nil,
+        total_fat: nil,
+        trans_fat: nil,
+        unsaturated_fat: nil
+      }
+    end
+
+    its(:instructions) { should == nil }
+    its(:prep_time) { should == nil }
+    its(:published_date) { should == Date.parse("2010-07-01") }
+    its(:total_time) { should == nil }
+    its(:yield) { should == "20" }
+
+  end
+
+end
